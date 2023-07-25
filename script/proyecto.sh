f5bdcdc5f3ef9220d164fa28c3c9086687e4004e
theleywin <123096717+theleywin@users.noreply.github.com>
2023-07-25 23:39:10 +0200

    finaly

--- script/proyecto.sh
+++ script/proyecto.sh
@@ -34,7 +34,7 @@ report(){
                                             
     cd ../
     cd Informe
-    pdflatex -synctex=1 -interaction=nonstopmode Informe.tex
+    pdflatex -synctex=1 -interaction=nonstopmode informe.tex
 }
 
 slides(){
@@ -52,80 +52,141 @@ slides(){
 \e[45m                                                                                                                       \e[49m"
     cd ../
     cd Presentacion
-    pdflatex -synctex=1 -interaction=nonstopmode Presentacion.tex
+    pdflatex -synctex=1 -interaction=nonstopmode presentacion.tex
 }
 
 show_report(){
     cd ..
     cd Informe
-    archivo="Informe.pdf"
+    archivo="informe.pdf"
 
     if [ ! -e "$archivo" ]; then
         report
-        xdg-open Informe.pdf
+        xdg-open informe.pdf
     else
-        xdg-open Informe.pdf
+        xdg-open informe.pdf
     fi
 }
 
-show_report(){
+show_slides(){
     cd ..
     cd Presentacion
-    archivo="Presentacion.pdf"
+    archivo="presentacion.pdf"
 
     if [ ! -e "$archivo" ]; then
         slides
-        xdg-open Presentacion.pdf
+        xdg-open presentacion.pdf
     else
-        xdg-open Presentacion.pdf
+        xdg-open presentacion.pdf
     fi
 }
 
 clean(){
     cd ../
     cd Informe
-    rm -v Informe.aux
-    rm -v Informe.log
-    rm -v Informe.out
-    rm -v Informe.pdf
-    rm -v Informe.synctex.gz
-    rm -v Informe.toc
+    rm -v informe.aux
+    rm -v informe.log
+    rm -v informe.out
+    rm -v informe.pdf
+    rm -v informe.synctex.gz
+    rm -v informe.toc
 
     cd ../
     cd Presentacion
-    rm -v Presentacion.aux
-    rm -v Presentacion.log
-    rm -v Presentacion.out
-    rm -v Presentacion.pdf
-    rm -v Presentacion.synctex.gz
+    rm -v presentacion.aux
+    rm -v presentacion.log
+    rm -v presentacion.out
+    rm -v presentacion.pdf
+    rm -v presentacion.synctex.gz
     rm -v texput.log
+    rm -v presentacion.toc
+    rm -v presentacion.nav
+    rm -v presentacion.snm
 }
 
+menu(){
+echo "\e[45m                                                                                                          \e[49m
+
+    /\/\   ___   ___   __ _| | ___       
+   /    \ / _ \ / _ \ / _  | |/ _ \      
+  / /\/\ \ (_) | (_) | (_| | |  __/_ _ _ 
+  \/    \/\___/ \___/ \__, |_|\___(_|_|_)
+
+\e[45m                                                                                                          \e[49m"
+
+echo "Choose an option:
+Run - corre el proyecto
+Report - compila el latex del informe
+Slides - compila el latex de la presentación
+Show_report - revisa a ver si ya está creado el pdf del informe, y sino, ejecuta report (compila el latex)
+Show_slides - revisa a ver si ya está creado el pdf de la presentacion, y sino, ejecuta slides (compila el latex)
+Clean - limpia de las carpetas Presentacion e Informe todo lo que no sea .tex
+"
+read var
+
+if [ $var = "run" ]; then
+    clear
+    run
+
+elif [ $var = "report" ]; then
+    clear
+    report
+
+elif [ $var = "slides" ]; then
+    clear
+    slides
+
+elif [ $var = "show_report" ]; then
+    clear
+    show_report
+
+elif [ $var = "show_slides" ]; then
+    clear
+    show_slides
+
+elif [ $var = "clean" ]; then
+    clear
+    clean
+fi
+}
+
+
+selector(){
 
 if [ $1 = "run" ]; then
     clear
     run
-fi
 
-if [ $1 = "report" ]; then
+elif [ $1 = "report" ]; then
     clear
     report
-fi
-if [ $1 = "slides" ]; then
+
+elif [ $1 = "slides" ]; then
     clear
     slides
-fi
-if [ $1 = "show_report" ]; then
+
+elif [ $1 = "show_report" ]; then
     clear
     show_report
-fi
-if [ $1 = "show_slides" ]; then
+
+elif [ $1 = "show_slides" ]; then
     clear
-    show_report
-fi
-if [ $1 = "clean" ]; then
+    show_slides
+
+elif [ $1 = "clean" ]; then
     clear
     clean
+
+else 
+    menu
 fi
 
+}
+
+if [ ! $1 = null ]; then
+    selector $1
+else
+    menu
+
+fi
 
